ce24f46abc9ea822ea2ab13fca35f9d1d0724c16
René Laqua <renelaqua@Rene-Laquas-MBP.fritz.box>
2022-01-14 18:25:06 +0100

    initial commit, empty template

---
+++ Multifidus_AtrophyFilter.h
@@ -0,0 +1,17 @@
+//
+//  Multifidus_AtrophyFilter.h
+//  Multifidus_Atrophy
+//
+//  Copyright (c) 2022 René Laqua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Horos/PluginFilter.h>
+
+@interface Multifidus_AtrophyFilter : PluginFilter {
+
+}
+
+- (long) filterImage:(NSString*) menuName;
+
+@end
---
+++ Multifidus_AtrophyFilter.m
@@ -0,0 +1,28 @@
+//
+//  Multifidus_AtrophyFilter.m
+//  Multifidus_Atrophy
+//
+//  Copyright (c) 2022 René Laqua. All rights reserved.
+//
+
+#import "Multifidus_AtrophyFilter.h"
+
+@implementation Multifidus_AtrophyFilter
+
+- (void) initPlugin
+{
+}
+
+- (long) filterImage:(NSString*) menuName
+{
+	ViewerController	*new2DViewer;
+	
+	// In this plugin, we will simply duplicate the current 2D window!
+	
+	new2DViewer = [self duplicateCurrent2DViewerWindow];
+	
+	if( new2DViewer) return 0; // No Errors
+	else return -1;
+}
+
+@end
